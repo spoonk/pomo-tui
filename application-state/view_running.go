9903020049b1cabb981039e8300ebda1c4299904
spoonk <cornwlucas@gmail.com>
2026-02-28 10:07:29 -0800

    add project to running UI

--- application-state/view_running.go
+++ application-state/view_running.go
@@ -13,11 +13,19 @@ func (m Model) runningSessionView() string {
 		keybinds = "p: resume · x: terminate · q: quit"
 	}
 
+	project := ""
+
+	if m.selectedProject != nil {
+		project = ui.ProjectTitle(m.selectedProject.Name)
+	}
+
 	pause := ui.PauseIndicator(m.sessionTimer.IsPaused())
 	timer := ui.TimerDisplay(m.sessionTimer.Elapsed(), m.sessionTimer.TimeLimit())
 	hints := ui.Keybinds(keybinds)
 	breakLabel := ui.DimLabel(fmt.Sprintf(" - (break: %s)", ui.FormatDuration(m.breakTimer.TimeLimit())))
 
-	content := pause + timer + breakLabel + "\n" + hints
+	content := lipgloss.JoinVertical(lipgloss.Center, project, pause+timer+breakLabel, "\n"+hints)
+
+	// content := pause + timer + breakLabel + "\n" + hints
 	return lipgloss.Place(m.width, m.height, lipgloss.Center, lipgloss.Center, content)
 }
